b26c6c92c5c1685e631ccc974ddacef8be2da5b1
microStationCorp <sujanmondal916@gmail.com>
2020-03-17 22:11:40 +0530

    class based done, get efficient subsidury puzzle

--- puzz_8.py
+++ puzz_8.py
@@ -16,76 +16,77 @@ FINAL_PUZ = np.array(
     ]
 )
 
+class get_subsidurry_puzzle:
 
-def blank_pos(input_array):
-    for i in range(len(input_array)):
-        for k in range(len(input_array[i])):
-            if input_array[i][k] == 0:
-                return (i, k)
+    def __init__(self, input_array, previous_blank_pos=None):
+        self.input_array = input_array
+        self.blank_pos_value = self.blank_pos_func()
+        self.previous_blank_pos = previous_blank_pos
+        self.valid_direction_value = self.valid_direction_func()
 
+    def swap(self, input1, input2):
+        temp = input1
+        input1 = input2
+        input2 = temp
+        return input1, input2
 
-def valid_direction(blank_pos, previous_blank_pos=None):
-    valid_direction_pos = []
-    if blank_pos[0] + 1 < ARRAY_SHAPE[0]:
-        valid_direction_pos.append([blank_pos[0] + 1, blank_pos[1]])
-    if blank_pos[0] - 1 >= 0:
-        valid_direction_pos.append([blank_pos[0] - 1, blank_pos[1]])
-    if blank_pos[1] + 1 < ARRAY_SHAPE[1]:
-        valid_direction_pos.append([blank_pos[0], blank_pos[1] + 1])
-    if blank_pos[1] - 1 >= 0:
-        valid_direction_pos.append([blank_pos[0], blank_pos[1] - 1])
-    if previous_blank_pos != None:
-        valid_direction_pos.pop(valid_direction_pos.index(previous_blank_pos))
-    return valid_direction_pos
+    def valid_direction_func(self):
+        valid_direction_pos = []
+        if self.blank_pos_value[0] + 1 < ARRAY_SHAPE[0]:
+            valid_direction_pos.append([self.blank_pos_value[0] + 1, self.blank_pos_value[1]])
+        if self.blank_pos_value[0] - 1 >= 0:
+            valid_direction_pos.append([self.blank_pos_value[0] - 1, self.blank_pos_value[1]])
+        if self.blank_pos_value[1] + 1 < ARRAY_SHAPE[1]:
+            valid_direction_pos.append([self.blank_pos_value[0], self.blank_pos_value[1] + 1])
+        if self.blank_pos_value[1] - 1 >= 0:
+            valid_direction_pos.append([self.blank_pos_value[0], self.blank_pos_value[1] - 1])
+        if self.previous_blank_pos != None:
+            valid_direction_pos.pop(valid_direction_pos.index(self.previous_blank_pos))
+        return valid_direction_pos
 
+    def blank_pos_func(self):
+        for i in range(len(self.input_array)):
+            for k in range(len(self.input_array[i])):
+                if self.input_array[i][k] == 0:
+                    return (i, k)
 
-def swap(input1, input2):
-    temp = input1
-    input1 = input2
-    input2 = temp
-    return input1, input2
+    def difference_count(self, input_array):
+        b = FINAL_PUZ != input_array
+        return len(input_array[b])
 
+    def efficient_arrays(self, list_of_arrays):
+        arrays = []
+        for l in list_of_arrays:
+            if self.difference_count(l) == 0:
+                return l
+            arrays.append({
+                "difference": self.difference_count(l),
+                "array": l,
+                "blank_pos": self.blank_pos_value
+            })
+        arrays = sorted(arrays, key=lambda i: i['difference'])
+        eff_arrays = []
+        for a in arrays:
+            if a['difference'] == arrays[0]['difference']:
+                eff_arrays.append(a)
+        return eff_arrays
 
-def new_test_puzzles(input_array, valid_direction_pos, blank_pos):
-    valid_puzzles = []
-    for vp in valid_direction_pos:
-        input_array[vp[0]][vp[1]], input_array[blank_pos[0]][blank_pos[1]] = swap(input_array[vp[0]][vp[1]],
-                                                                                  input_array[blank_pos[0]][
-                                                                                      blank_pos[1]])
-        valid_puzzles.append(input_array.copy())
-        input_array[vp[0]][vp[1]], input_array[blank_pos[0]][blank_pos[1]] = swap(input_array[vp[0]][vp[1]],
-                                                                                  input_array[blank_pos[0]][
-                                                                                      blank_pos[1]])
-    return valid_puzzles
+    def new_test_puzzles(self):
+        valid_puzzles = []
+        for vp in self.valid_direction_value:
+            self.input_array[vp[0]][vp[1]], self.input_array[self.blank_pos_value[0]][
+                self.blank_pos_value[1]] = self.swap(
+                self.input_array[vp[0]][vp[1]], self.input_array[self.blank_pos_value[0]][self.blank_pos_value[1]])
+            valid_puzzles.append(self.input_array.copy())
+            self.input_array[vp[0]][vp[1]], self.input_array[self.blank_pos_value[0]][
+                self.blank_pos_value[1]] = self.swap(
+                self.input_array[vp[0]][vp[1]], self.input_array[self.blank_pos_value[0]][self.blank_pos_value[1]])
+        return valid_puzzles
 
+    def main(self):
+        list = self.new_test_puzzles()
+        arrays = self.efficient_arrays(list)
+        return arrays
 
-def difference_count(input_array):
-    b = FINAL_PUZ != input_array
-    return len(input_array[b])
-
-
-def efficient_arrays(list_of_arrays):
-    arrays = []
-    for l in list_of_arrays:
-        if difference_count(l) == 0:
-            return l
-        arrays.append({
-            "difference": difference_count(l),
-            "array": l
-        })
-    arrays = sorted(arrays, key=lambda i: i['difference'])
-    eff_arrays = []
-    for a in arrays:
-        if a['difference'] == arrays[0]['difference']:
-            eff_arrays.append(a)
-    return eff_arrays
-
-
-def main(input_array):
-    list = new_test_puzzles(input_array.copy(), valid_direction(blank_pos(input_array)), blank_pos(input_array))
-    arrays = efficient_arrays(list)
-    return arrays
-
-
-print(main(INIT_PUZ.copy()))
-print('sujan')
+gsp = get_subsidurry_puzzle(INIT_PUZ.copy())
+print(gsp.main())
